7c3aa976db4aa057297ba59c140d0cc672a3d324
Jason Pang <jason@jasonhpang.com>
2024-06-22 22:35:03 -0700

    Remove websocket messaging code from frontend app

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,43 +1,10 @@
-import Head from "next/head";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { TestComponent } from "@pianocheat/ui";
-import { useCallback, useEffect, useRef } from "react";
-import { MessageClient } from "@pianocheat/messaging.client";
+import { Inter } from "next/font/google";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const messageClientRef = useRef<MessageClient | null>(null);
-
-  const onRequestMessage = useCallback((id: string, payload: any) => {
-    console.log("[Message Request]", id, payload);
-  }, []);
-
-  const onConnected = useCallback(async (client: MessageClient) => {
-    console.log("[Socket Connected]");
-    client.sendEvent("Event from client");
-    const response = await client.sendRequest("test");
-    console.log("[Response from test request]", response);
-  }, []);
-
-  const onEvent = useCallback((id: string, payload: any) => {
-    console.log("[Message Event]", id, payload);
-  }, []);
-
-  useEffect(() => {
-    messageClientRef.current = new MessageClient({
-      onConnected,
-      onRequestMessage,
-      onEvent,
-    });
-    messageClientRef.current.connect();
-    (window as any)._socket = messageClientRef.current;
-
-    return () => {
-      messageClientRef.current?.disconnect();
-    };
-  }, [onConnected, onEvent, onRequestMessage]);
-
   return (
     <>
       <Head>
@@ -46,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        Hello world!
         <TestComponent />
       </main>
     </>
